82914e7a29fca163b0eafe68e693deb51c11d501
peterplug <foofooxdude@gmail.com>
2025-01-18 19:40:23 +0100

    Update peterplug-blur-app.py
    fix for 60fps video rendering in 30fps

--- src/peterplug-blur-app.py
+++ src/peterplug-blur-app.py
@@ -8,8 +8,8 @@ class PeterplugBlurApp:
     def __init__(self, root):
         self.root = root
         self.root.title("peterplug blur app")
-        self.root.geometry("560x145")
-        self.root.resizable(False, False)  # make the window non-resizable
+        self.root.geometry("660x160")
+        self.root.resizable(False, False)
         self.input_file = None
         self.process = None
         self.frame_rate = tk.StringVar()
@@ -71,7 +71,7 @@ class PeterplugBlurApp:
             os.remove(output_file)
 
         # create the ffmpeg command
-        command = f"ffmpeg -i {self.input_entry.get()} -vf 'scale={self.resolution.get()}, minterpolate=mi_mode=mci:mc_mode=aobmc:vsbmc=1, tblend=all_mode=average, framestep=2' -pix_fmt yuv420p -b:v 24M -r {self.frame_rate.get()} {output_file}"
+        command = f"ffmpeg -i {self.input_entry.get()} -vf 'scale={self.resolution.get()}, minterpolate=mi_mode=mci:mc_mode=aobmc:vsbmc=1, tblend=all_mode=average' -pix_fmt yuv420p -b:v 24M -r {self.frame_rate.get()} {output_file}"
 
         # run the ffmpeg command in a separate thread
         self.thread = threading.Thread(target=self.run_ffmpeg, args=(command, output_file))
